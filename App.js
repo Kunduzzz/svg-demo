bf70a24c4bbfdb181c2d6b7d2e3942b04c856d38
Priyanshu Kundu <priyanshu.kundu@truworthwellness.com>
2023-02-23 19:42:22 +0530

    Strict

--- App.js
+++ App.js
@@ -5,62 +5,25 @@
  * @format
  */
 
-import React, { useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
-import DocumentPicker from 'react-native-document-picker';
-import TestSvg from './assets/ask-doctor.svg'
-import Svg, { Circle } from 'react-native-svg'
-import Slider from 'react-native-slider'
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, View } from 'react-native';
+import Svg, { Circle } from 'react-native-svg';
+import Slider from 'react-native-slider';
+
 const App = () => {
-  const [selectedFile, setselectedFile] = useState(null)
-  const [sliderVal, setSliderVal] = useState(0)
-  const [File, setFile] = useState(null)
-  const selectDocument = async () => {
-    const file = await DocumentPicker.pick({
-      type: [DocumentPicker.types.allFiles],
-    });
-    if (file) {
-      setselectedFile(file[0]);
-    }
-  };
+  const [sliderVal, setSliderVal] = useState(0);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle={'light-content'} backgroundColor="#209797" />
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        {selectedFile &&
-          <>
-            <Text style={{ fontSize: 20, fontWeight: 'bold', color: "#000" }}>
-              {selectedFile.name}
-            </Text>
-            <Text style={{ fontSize: 20, fontWeight: 'bold', color: "#000" }}>
-              {''}
-            </Text>
-          </>
-        }
-
-        <TouchableOpacity
-          activeOpacity={0.8}
-          onPress={selectDocument}
-          style={{ padding: 15, backgroundColor: "#209797", borderRadius: 10, elevation: 5 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: "#fff" }}>
-            Select SVG
-          </Text>
-        </TouchableOpacity>
 
         <Slider
           value={sliderVal}
           minimumValue={1}
           maximumValue={10}
           onValueChange={setSliderVal}
-          style={{ width: '80%' }} />
+          style={{ width: '50%' }} />
 
         <Svg
           viewBox="0 0 100 100"
